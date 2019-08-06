f0cfe1c939899155665fa736919ae7c3fbb6656e
Omar Santos <santosomar@gmail.com>
2019-08-06 15:17:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo "
 ╚███╔███╔╝███████╗██████╔╝███████║██║     ███████╗╚██████╔╝██║   ██║
  ╚══╝╚══╝ ╚══════╝╚═════╝ ╚══════╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝   ╚═╝
 
-https://websploit.h4cker.org
+https://websploit.org
 Author: Omar Ωr Santos
 Version: 2.0
 
@@ -28,24 +28,36 @@ read -n 1 -s -r -p "Press any key to continue the setup..."
 
 echo " "
 
+# Setting Up vim with Python Jedi to be used in several training courses 
 
-apt-get update
+cd ~/
+apt update
+apt install -y wget
+apt install -y vim
+apt install -y vim-python-jedi
+apt install -y curl vim exuberant-ctags git ack-grep 
+pip install pep8 flake8 pyflakes isort yapf
+
+# Then get the .vimrc file from my repo:
+curl https://raw.githubusercontent.com/The-Art-of-Hacking/websploit/master/.vimrc > ~/.vimrc
+
 #installing Docker
 apt-get install -y docker.io
 
-#instantiating Docker containers
-docker run --name hackazon -d --restart unless-stopped -p 80:80 santosomar/hackazon
+# setup containers
 docker run --name webgoat -d --restart unless-stopped -p 6661:8080 -t santosomar/webgoat
 docker run --name juice-shop --restart unless-stopped -d -p 6662:3000 santosomar/juice-shop
 docker run --name dvwa --restart unless-stopped -itd -p 6663:80 santosomar/dvwa
 docker run --name mutillidae_2 --restart unless-stopped -d -p 6664:80 santosomar/mutillidae_2
-docker run --name bwapp2 --restart unless-stopped -d -p 8081:80 santosomar/bwapp
+docker run --name bwapp2 --restart unless-stopped -d -p 6665:80 santosomar/bwapp
+docker run --name dvna --restart unless-stopped -d -p 6666:9090 santosomar/dvna
+docker run --name hackazon -d --restart unless-stopped -p 6667:80 santosomar/hackazon
 
-#bwapp - go to /install.php then user/pass is bee/bug
-#docker run --name vapp2 --restart unless-stopped -d -p 9090:80 santosomar/vuln_app
+# for bwapp - go to /install.php then user/pass is bee/bug
+# not ready for prod: 
+# docker run --name vapp2 --restart unless-stopped -d -p 9090:80 santosomar/vuln_app
 
 #setting up look and feel
-
 cd /root/Pictures
 wget https://h4cker.org/img/h4cker_wallpaper.png
 gsettings set org.gnome.desktop.background picture-uri "file:///root/Pictures/h4cker_wallpaper.png"
